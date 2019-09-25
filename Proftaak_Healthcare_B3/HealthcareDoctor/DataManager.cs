bfd7bdbe772201b169a30b96b390da738ad608fd
LeviVlasblom <D168228@edu.rocwb.nl>
2019-09-25 13:45:01 +0200

    Login message naar de server

---
+++ Proftaak_Healthcare_B3/HealthcareDoctor/DataManager.cs
@@ -0,0 +1,22 @@
+﻿using Networking.HealthCare;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareDoctor
+{
+    class DataManager
+    {
+        public DataManager()
+        {
+
+        }
+
+        public void SendLogin()
+        {
+            Message message
+        }
+    }
+}
