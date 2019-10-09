0545fe9839f967197ae0db55d378dd0a97da467e
phunderr <pm.holthuijsen@student.avans.nl>
2019-10-09 13:25:46 +0200

    [added] Encryption class [added] unit test

---
+++ Proftaak_Healthcare_B3/projectTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography;
+using Networking.Server;
+
+namespace projectTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        //test for encryptor
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string data = "This is a test message";
+            string encrypted = Cryptography.Encrypt(data, "testpass");
+            string decrypted = Cryptography.Decrypt(encrypted, "testpass");
+
+            Assert.AreEqual(data, decrypted);
+
+        }
+    }
+}
